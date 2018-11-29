6afb940a4978f7df2d277b703dd10aa251fca5d5
lip <lip136@potevio.com>
2018-11-29 09:55:06 +0800

    14longestCommonPrefix

---
+++ 14_longestCommonPrefix.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        # 便利第一个str，查看是否在每一个字符串里面即可
+
+
+        if not strs:
+            return ""
+        small_str = min(strs, key=len)
+        for i, c in enumerate(small_str):
+            for str_l in strs:
+                if str_l[i] != c:
+                    return small_str[:i]
+        return small_str
